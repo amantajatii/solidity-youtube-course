1402b6bcbbac84d4ae2528a6d0d1430cae5f32f6
amantajatii <diazamantajatisusilo@mail.ugm.ac.id>
2025-05-17 08:01:45 +0700

    17-done

--- contracts/ultimate-solidity-course-for-complete-beginners-main/Exercises/17-Twitter-Modifier-Exercise.sol
+++ contracts/ultimate-solidity-course-for-complete-beginners-main/Exercises/17-Twitter-Modifier-Exercise.sol
@@ -10,7 +10,7 @@ pragma solidity ^0.8.0;
 
 contract Twitter {
 
-    uint16 constant MAX_TWEET_LENGTH = 280;
+    uint16 public MAX_TWEET_LENGTH = 280;
 
     struct Tweet {
         address author;
@@ -19,6 +19,20 @@ contract Twitter {
         uint256 likes;
     }
     mapping(address => Tweet[] ) public tweets;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can modify the max tweet length!");
+        _;
+    }
+
+    function changeTweetLength (uint16 newMaxTweetLength) public onlyOwner {
+        MAX_TWEET_LENGTH = newMaxTweetLength;
+    }
 
     function createTweet(string memory _tweet) public {
         require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet is too long bro!" );
